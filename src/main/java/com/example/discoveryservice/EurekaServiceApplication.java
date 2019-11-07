846bd7cf394d4795460af9711288b111ef0393da
AnandMohan123 <anandmohan123@gmail.com>
2019-11-07 18:10:09 +0800

    Commit again

--- src/main/java/com/example/discoveryservice/EurekaServiceApplication.java
+++ src/main/java/com/example/discoveryservice/EurekaServiceApplication.java
@@ -1,8 +1,11 @@
 package com.example.discoveryservice;
 
 import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
 
 @EnableEurekaServer
 @SpringBootApplication
@@ -11,5 +14,6 @@ public class EurekaServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaServiceApplication.class, args);
 	}
+
 // this my change
 }
